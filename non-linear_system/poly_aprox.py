b2a5f92dce6e1c050ed718c3251ca8bf3f3ea427
benjaSantana <0211518@up.edu.mx>
2020-09-30 08:53:17 -0500

    Fixed error in poly_aprox

--- non-linear_system/poly_aprox.py
+++ non-linear_system/poly_aprox.py
@@ -21,21 +21,23 @@ x = sp.Symbol('x')
 ## Input #####
 ##################
 
-fx = sp.cos(x)
-max_iter = 15
+#fx = sp.cos(x)
+fx = math.e**x
+#fx = sp.sin(x) + sp.cos(2*x)
+max_iter = 5
 x0 = 0
 
 ###############
 ## Algorithm #
 ###############
 pn = 0
+fxprime = fx
 for i in range(0,max_iter):
-    fxprime = fx.diff(x)
-    #print(fxprime)
     feval = fxprime.evalf(subs={x: x0})
     fact = math.factorial(i)
     wx = (x-x0)**i
     pn += (feval/fact)*(wx) 
+    fxprime = fx.diff(x)
     fx = fxprime
     #print(pn)
 
